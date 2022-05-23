2d09e3194dd2b3e5a04c52459b15567f2e645b1b
Swirek3331 <82308691+Swirek3331@users.noreply.github.com>
2022-05-23 17:06:52 +0100

    Create Liquids.java

---
+++ src/altanic/content/Liquids.java
@@ -0,0 +1,50 @@
+package altanic.content;
+
+import arc.graphics.Color;
+import mindustry.type.*;
+
+public class Liquids
+{
+    public static Liquid
+    //region alcochols
+    methanol, ethanol,
+
+    //region acids
+    sulfuricAcid, nitrogenAcid, chloricAcid,
+
+    //region gases
+    oxygen, deuterium, tritium, hellium, helliumThree, steam,
+
+    //region oils aka fuels
+    lightOil, heavyOil, refinedFuel, gas, plantOil, plantGas, biofuel,
+
+    //region colers
+    liquidNitrogen, liquidHelliumm, lubricant, coil,
+
+    //region other
+    lava;
+
+    static float waterBoilingPoint = 0.5f;
+
+    public static void load()
+    {
+
+        //region alcochols
+
+            methanol = new Liquid("methanol", Color.valueOf("ffffff"))
+            {{
+                heatCapacity = 0.4f;
+                boilPoint = waterBoilingPoint - waterBoilingPoint * 0.25f;
+                gasColor = Color.valueOf("ffffff");
+            }};
+
+            ethanol = new Liquid("ethanol", Color.valueOf("ffffff"))
+            {{
+                heatCapacity = 0.4f;
+                boilPoint = waterBoilingPoint - waterBoilingPoint * 0.25f;
+                gasColor = Color.valueOf("ffffff");
+            }};
+        
+        //endregion
+    }
+}
