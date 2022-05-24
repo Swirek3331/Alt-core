27d8bb1ca4537d53c06df1d7dc43712122afe54a
Swirek3331 <82308691+Swirek3331@users.noreply.github.com>
2022-05-24 14:46:35 +0100

    coś nie chce działać

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Altanic
-"Debile są niczym sezonowa grypa
- Cały czas wracają i niemiłosiernie trują dupę"
+"Debile są niczym sezonowa grypa. Cały czas wracają i niemiłosiernie trują dupę"
 -Omnom aka Baron Cukierkowy
 # NOT READY YET
 ![](https://github.com/Swirek3331/Altanic/blob/master/assets/sprites/github/frog.png)
--- src/altanic/Altanic.java
+++ src/altanic/Altanic.java
@@ -1,27 +1,29 @@
 package altanic;
 
-/* chyba nie jest potrzbne import arc.*;
+import arc.*;
 import arc.util.*;
-import mindustry.game.EventType.*;*/
+import mindustry.game.EventType.*;
 import mindustry.mod.*;
-//import mindustry.ui.dialogs.*;
+import mindustry.ui.dialogs.*;
 
 import altanic.content.*;
+import altanic.content.AltanicBlocks.*;
+import altanic.content.AltanicLiquids.*;
+import altanic.content.AtalnicErekirTechTree.*;
 
 public class Altanic extends Mod
 {
-        public Altanic()
-        {
+    public Altanic()
+    {
 
-        }
+    }
         
-        @Override
+    @Override
 
-        public void loadContent()
-        {
-            AltanicBlocks.load();
-            AltanicLiquids.load();
-            //AltanicItems.load();
-        }
+    public void loadContent()
+    {
+        AltanicBlocks.load();
+        AltanicLiquids.load();
+        AltanicErekirTechTree.load();
+    }
 }
-
