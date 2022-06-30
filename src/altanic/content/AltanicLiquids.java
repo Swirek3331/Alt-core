3614686f39817314f86388094ff85d0892cbff79
Swirek3331 <82308691+Swirek3331@users.noreply.github.com>
2022-06-30 16:26:31 +0100

    Update AltanicLiquids.java

--- src/altanic/content/AltanicLiquids.java
+++ src/altanic/content/AltanicLiquids.java
@@ -20,7 +20,7 @@ public class AltanicLiquids
     lightOil, heavyOil, refinedFuel, naturalGas, petroleumGas, plantOil, bioGas, biofuel,
 
     //region coolants
-    liquidNitrogen, liquidHelliumm, lubricant,
+    liquidNitrogen, liquidHelliumm,
 
     //region other
     lava, nitroglycerin;
@@ -71,33 +71,33 @@ public class AltanicLiquids
             
             oxygen = new Liquid("oxygen", Color.valueOf("ffffff"))
             {{
-                gas = true;
+
             }};
 
             deuterium = new Liquid("deuterium", Color.valueOf("0000ff"))
             {{
-                gas = true;
+
             }};
 
             tritium = new Liquid("tritium", Color.valueOf("ff00ff"))
             {{
-                gas = true;
+
             }};
 
             hellium = new Liquid("hellium", Color.valueOf("ffffff"))
             {{
-                gas = true;
+
             }};
 
             helliumThree = new Liquid("hellium-three", Color.valueOf("ffffff"))
             {{
-                gas = true;
+
             }};
 
             steam = new Liquid("steam", Color.valueOf("ffffff"))
             {{  
                 alwaysUnlocked = true;
-                gas = true;
+
             }};
 
         //fuels
@@ -119,12 +119,12 @@ public class AltanicLiquids
 
             naturalGas = new Liquid("natural-gas", Color.valueOf("ffffff"))
             {{  
-                gas = true;
+
             }};
 
             petroleumGas = new Liquid("petroleum-gas", Color.valueOf("ffffff"))
             {{  
-                gas = true;
+
             }};
 
             plantOil = new Liquid("plant-oil", Color.valueOf("ffffff"))
@@ -134,7 +134,7 @@ public class AltanicLiquids
 
             bioGas = new Liquid("biogas", Color.valueOf("ffffff"))
             {{
-                gas = true;
+
             }};
 
             biofuel = new Liquid("biofuel", Color.valueOf("ffffff"))
